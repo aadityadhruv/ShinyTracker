e5dc445d2f728e67fbaca80cc1e7945816e1f813
aadityadhruv <aadityadhruv11@gmail.com>
2021-01-09 13:42:36 +0530

    refactor

--- GetImages.py
+++ GetImages.py
@@ -3,9 +3,8 @@ import pandas as pd
 import cv2 as cv
 import cv2
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
 import time
-from pytube import YouTube
+
 import os
 import shutil
 import math
@@ -13,37 +12,9 @@ import datetime
 import random
 
 
-
-
-videofile='werster_white_2_fast_2020.mp4'
-video =cv2.VideoCapture(videofile)
-videofile2='werster_white_2_cm_2020.mp4'
-video2 =cv2.VideoCapture(videofile2)
-
-frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-frame_count2 = int(video2.get(cv2.CAP_PROP_FRAME_COUNT))
-
-
-for frame in random.sample(range(1000, frame_count - 1000)):
-
-    is_good, pFrame = processFrame(video, frame)
-    if is_good:
-        saveFrame(directory, pFrame, frame,  suffix)
-
-for frame in random.sample(range(1000, frame_count - 1000)):
-
-    is_good, pFrame = processFrame(video2, frame)
-    if is_good:
-        saveFrame(directory, pFrame, frame, suffix)
-
-
-renameDirectory(directory, suffix)
-
-
-
-def processFrame(video, frame, thresh1=100, thresh2=600):
-    video.set(cv2.CAP_PROP_POS_MSEC,frame) 
-    hasFrames,image = vidcap.read()
+def processFrame(video, sec, thresh1=100, thresh2=600):
+    video.set(cv2.CAP_PROP_POS_MSEC,sec*1000) 
+    hasFrames,image = video.read()
     is_good = False
     if hasFrames:
         image = image[:-45, 375:]
@@ -57,13 +28,13 @@ def processFrame(video, frame, thresh1=100, thresh2=600):
         
         dominant = palette[np.argmax(counts)]
         
-        if (np.sum(dominant) > thresh1 && np.sum(dominant) < thresh2):
+        if (np.sum(dominant) > thresh1 and np.sum(dominant) < thresh2):
             is_good = True 
     
     return is_good, image
 
-def saveFrame(directory="images/", frame, index,  suffix="f"):
-    cv2.imwrite(directory + suffix + str(index) + ".png")
+def saveFrame(frame, index, directory="test_images/", suffix="f"):
+    cv2.imwrite(directory + suffix + str(index) + ".png", frame)
 
 
 
@@ -78,4 +49,35 @@ def renameDirectory(directory ,suffix="f"):
         os.rename(src, dst) 
 
 
+videofile='werster_white_2_fast_2020.mp4'
+video =cv2.VideoCapture(videofile)
+videofile2='werster_white_2_cm_2020.mp4'
+video2 =cv2.VideoCapture(videofile2)
 
+frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_count2 = int(video2.get(cv2.CAP_PROP_FRAME_COUNT))
+
+fps = video.get(cv2.CAP_PROP_FPS)
+fps2 = video2.get(cv2.CAP_PROP_FPS)
+duration = frame_count/fps
+duration2 = frame_count2/fps2
+
+
+samples = 1000
+
+
+directory = "test_images/"
+suffix = "video1"
+for frame in random.sample(range(500, np.int(duration) - 1000), samples):
+    is_good, pFrame = processFrame(video, frame)
+    if is_good:
+        saveFrame(pFrame, frame, directory, suffix)
+suffix = "video2"
+for frame in random.sample(range(1000, np.int(duration2) - 1000), samples):
+
+    is_good, pFrame = processFrame(video2, frame)
+    if is_good:
+        saveFrame(pFrame, frame, directory, suffix)
+
+
+renameDirectory(directory)
